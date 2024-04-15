c463118b4a933f43e2dd2fdd8e98978dad390a71
preettypeanuts <mrafly212001@gmail.com>
2024-04-15 20:22:43 +0700

    pliss

--- src/Components/RegistrasiPage/BodyRegistrasi.jsx
+++ src/Components/RegistrasiPage/BodyRegistrasi.jsx
@@ -6,7 +6,7 @@ export const BodyRegistrasi = () => {
             <section className="lg:px-24 px-5 pt-24">
                 <p className="text-xl !capitalize">
                     PENDAFTARAN PROGRAM PELATIHAN KERJA KE JEPANG LPK HIDAMARI KENSHU SENTA
-                </p>      
+                </p>
                 <RegisterForm />
             </section>
         </>
